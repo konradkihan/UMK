d7fa6d085923f9fb63e42e35dda32f928ce4f71c
Konrad Kihan <konrad.kihan@gmail.com>
2021-11-09 16:48:46 +0100

    bubble sort

--- lekcja_3/wprowadzenie.cpp
+++ lekcja_3/wprowadzenie.cpp
@@ -14,26 +14,50 @@ void tablice(){
         cout<<A[i]<<" ";
 }
 
-/* algorytm sortowania bombelkowego
-polega on na przenoszeniu największych elementów na koniec tablicy,
-najprostszy ale najbardziej wydajny */
-void sortowanie_bombelkowe(){
 
+void sortowanie_bombelkowe(){
+    /* algorytm sortowania bombelkowego
+    polega on na przenoszeniu największych elementów na koniec tablicy,
+    najprostszy ale najbardziej wydajny */
+    #pragma region generowanie tablicy z losowymi liczbami [0; 1]
+        const int N = 100000;
+        double tab[N];
+        srand(time(NULL));
+
+        for(int i = 0; i < N; i++)
+            tab[i] = 1.0 * rand() / RAND_MAX;
+
+        cout << "Tablica nieposortowana wygenerowana." << endl;
+    #pragma endregion
+
+    #pragma region algorytm sortownaia
+        int pom; // delkaracja zmiennej pomocniczej
+        for(int i = 0; i < N-1; i++){
+            for(int j = 0; j < N-i; j++){
+                if(tab[i] > tab[i+1]){
+                    pom = tab[j];
+                    tab[j] = tab[j+1];
+                    tab[j+1] = pom;
+                }
+            }
+        }
+        cout << "Tablica została posortowana.";
+    #pragma endregion
 }
 
-/* algorytm sortowania przez selekcję 
-Przebieg działania w tablicy n elementowej:
-START
-    wybieranie najmniejszego elementu w zakresie [0; n] i przeniesienie go na 0 pozycję,
-    wybieranie najmniejszego elementu w zakresie [1; n] i przeniesienie go na 1 pozycję, 
-    ...
-    wybieranie najmniejszego elementu w zakresie [n-2; n] i przeniesienie go na n-2 pozycję 
-    wybieranie najmniejszego elementu w zakresie [n-1; n] i przeniesienie go na n-1 pozycję
-STOP
-Algorytm wykonuje sie n-1 razy dla tablicy n elementowej. Nie należy do szybkich algorytmów,
-ale jest prosty w implementacji*/
-void sortowanie_selekcja(){
 
+void sortowanie_selekcja(){
+    /* algorytm sortowania przez selekcję 
+    Przebieg działania w tablicy n elementowej:
+    START
+        wybieranie najmniejszego elementu w zakresie [0; n] i przeniesienie go na 0 pozycję,
+        wybieranie najmniejszego elementu w zakresie [1; n] i przeniesienie go na 1 pozycję, 
+        ...
+        wybieranie najmniejszego elementu w zakresie [n-2; n] i przeniesienie go na n-2 pozycję 
+        wybieranie najmniejszego elementu w zakresie [n-1; n] i przeniesienie go na n-1 pozycję
+    STOP
+    Algorytm wykonuje sie n-1 razy dla tablicy n elementowej. Nie należy do szybkich algorytmów,
+    ale jest prosty w implementacji*/
     #pragma region  generowanie tablicy z losowymi liczbami [0; 1]
         const int N = 100000;
         double tab[N];
