3c8fa039be942d12cd2088c09bd78769579b3f2f
Konrad Kihan <konrad.kihan@gmail.com>
2021-11-22 18:57:20 +0100

    rekurencja

---
+++ lekcja_5/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+/* Napisać programy rozwiązujące w sposób rekurencyjny następujące problemy */
+unsigned long long cw51a(int n){
+    // Obliczanie silni
+    if (n==0) return 1;
+    return n * cw51a(n-1);
+}
+
+
+unsigned long long cw51b(int n, int k){
+    // Obliczanie symbolu newtona
+    if (k == 0 || k == n) return 1;
+    return cw51b(n-1, k-1) + cw51b(n-1, k);
+}
+
+unsigned long long cw51c(int n){
+    // Obliczanie n-tego wyrazu ciągu Fibonacciego
+    if(n == 1 || n == 2) return 1;
+    return cw51c(n-2) + cw51c(n-1);
+}
+
+unsigned long long cw51d(int n, int b){
+    // Algorytm euklidesa
+    if(b == 0) return n;
+    return cw51d(b, n % b);
+}
+
+unsigned long long cw51e(int n, int m){
+    // potęgowanie liczb naturalnych
+    // pow(n, m)
+    if(m == 0) return 1;
+    return n * cw51e(n, m-1);
+}
+
+unsigned long long cw51f(int n){
+    // Obliczanie sum postaci 
+    // Sn = 1/2 + 2/3 + 3/4 + ... + n/(n+1)
+    if(n == 1) return 1;
+    return n/(n+1) + cw51f(n-1);
+    // FIXME funkcja zawsze zwraca 1
+}
+
+unsigned long long cw51g(int n){
+    // Wyznaczanie liczby cyfr w podanej liczbie naturalnej
+    if (n < 10) return 1;
+    return cw51g(n / 10) + 1;
+}
+
+unsigned long long cw51h(){
+    // TODO Wieże Hanoi
+}
+unsigned long long cw51i(){
+    // TODO Wyszukiwanie binarne. Dane są uporządkowana lista i pojedynczy element.
+    // Należy sprawdzić czy element występuje na liście i zwrócić jego indeks.
+    // Jeżeli nie występuje zwracamy −1.
+}
+unsigned long long cw51j(int n){
+    // Ile kawałków pizzy można uzyskać za pomocą n prostoliniowych cięć nożem? 
+    // (Bardziej „matematycznie” — jaka jest największa liczba Ln obszarów wyznaczonych przez n
+    // prostych na płaszczyźnie?)
+    if(n == 1) return 2;
+    return n + cw51j(n-1);
+}
+unsigned long long cw51k(){
+    // TODO Obliczanie wyznacznika macierzy (korzystając z rozwinięcia Laplace'a)
+}
+unsigned long long cw51l(int n){
+    // TODO generowanie wszystkiech permutacji zbioru n-elementowego
+}
+
+int main(){
+    cout<<cw51g(43243);
+}
