4a89f0d47b9681934acaa49be534510e5a082463
Konrad Kihan <konrad.kihan@gmail.com>
2022-01-22 17:09:05 +0100

    first stable version

--- projekt_zaliczenie/zal2/main.cpp
+++ projekt_zaliczenie/zal2/main.cpp
@@ -10,29 +10,24 @@ Konrad Kihan 2022
 #include<string>
 #include<sstream>
 
+
 using namespace std;
-
-// TODO zaimplementowac to_lower() aby wyszukiwalo slowa niezależnie od rozmiaru ich liter
-// TODO zaimplementować usuwanie przecinków i kropek aby nie były wliczane do słowa
-// TODO poprawić komentarze (ewentualnie popracować nad czytelnością kodu)
-
-
-// BUG naprawić komunikację z użytkownikiem kiedy raz podana nazwa pliku wejściowego zostaje zapisana na zawsze
 
 
 bool is_duplicate(string word, string tab[], string tab_lines[], int tab_size, int line_number){
     /* sprawdza duplikaty i jeśli istnieją dopisuje ich lokalizację w liście line_number */
     bool is_dupe = false;
+
     for(int i = 0; i < tab_size; i++){
         if(tab[i] == word){
-            tab_lines[i] += to_string(line_number);
+            tab_lines[i] += to_string(line_number); // jeśli duplikat - dodaj numer jego linii
             is_dupe = true;
             break;
         }
     }
+    
     return is_dupe;    
 }
-
 
 
 void make_data(string name, int tab_size, string words[], string words_lines[] ){
@@ -45,26 +40,26 @@ void make_data(string name, int tab_size, string words[], string words_lines[] )
     
     else{
         string str, line;
-        
-            
         int id = 0;
         int line_number = 1;
+
         while(getline(file, line)){
             stringstream ss(line);
             while(getline(ss, str, ' ')){
-                if(!is_duplicate(str, words, words_lines, id, line_number)){
-                    words_lines[id] += to_string(line_number);
-                    words[id] = str;
+                str[0] = tolower(str[0]); // konwersja słów na małe litery
+                if(str[str.size()-1] == '.' || str[str.size()-1] == ',') str.pop_back(); // usuwanie kropek i przecinków z końców wyrazów
+                if(!is_duplicate(str, words, words_lines, id, line_number)){ // sprawdzenie czy duplikat
+                    words_lines[id] += to_string(line_number); // dopisanie linii wystąpienia 
+                    words[id] = str; // dopisaenie do listy słów
                     id++;
                 }
-
-                
             }
             line_number++;
         }
         file.close();
     }
 }
+
 
 void sort_words(int tab_size, string words[], string words_lines[]){
     /* funkcja sortuje listę słów alfabetycznie */
@@ -77,6 +72,7 @@ void sort_words(int tab_size, string words[], string words_lines[]){
         }
     }
 }
+
 
 void console_presentation(int tab_size, string words[], string words_lines[]){
     /* funkcja prezentuje wynik działania programu do konsoli */
@@ -113,43 +109,47 @@ void save_to_file(string name, int tab_size, string words[], string words_lines[
 }
 
 
-
-
-
 int main(){
     const int TAB_SIZE = 1000;
     string words[TAB_SIZE];
     string words_lines[TAB_SIZE];
-    
 
     cout << "Program do generowania skorowidzow." << endl << endl;
 
     while(true){
         string in_file, out_file, yes_no;
+        // wybranie lokalizacji pliku wejściowego
         cout << "Wpisz lokalizacje / nazwe pliku, ktorego skorowidz ma byc utworzony:" << endl << ">>> ";
         cin >> in_file;
-        
+
+        // wybranie czy wynik ma zostać zapisany do osobnego pliku tekstowego
         cout << "Czy wynik dzialania programu ma byc zapisany do pliku? Wpisz 'tak' lub 'nie', 'q' aby wyjsc." << endl << ">>> ";
         cin >> yes_no;
         
         if(yes_no == "q") break;
         
         if(yes_no == "tak"){
+            // wybór pliku tekstowego do zapisu
             cout << "Jak ma nazywac sie plik do ktorego zapisany jest skorowidz?" << endl << ">>> ";
             cin >> out_file;
 
+            // wykonanie programu
             make_data(in_file, TAB_SIZE, words, words_lines);
             sort_words(TAB_SIZE, words, words_lines);
             save_to_file(out_file, TAB_SIZE, words, words_lines);
         }
         else if(yes_no == "nie"){
+            // alternatywne wykonanie programu
             make_data(in_file, TAB_SIZE, words, words_lines);
             sort_words(TAB_SIZE, words, words_lines);
             console_presentation(TAB_SIZE, words, words_lines);
         }
         else{
+            // gdy dane zostaną źle wprowadzone
             cout << "Niepoprawne dane.";
         }
+
+        // gdy skrypt wykona się poprawnie
         cout << "Wykonano!" << endl;
         
     }
