902d50b810a177f1d6e92d8bbc88b395c2c1687f
Konrad Kihan <konrad.kihan@gmail.com>
2022-04-06 15:38:16 +0200

    PHP init

---
+++ Projektowanie_stron_www-r1s2/Projekt_zaliczenie/add_to_database.php
@@ -0,0 +1,13 @@
+<?php
+    
+    function save_to_db(
+        $host = "localhost";
+        $user = "root";
+        $haslo = "";
+        $db = "umkwww";
+    ){
+        echo("A")
+    }
+    
+    // TODO make database and connect file with js function
+?>
