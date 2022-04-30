bc2b2cb0c2dfdfa6276d6dda338fc27698159c9c
Konrad Kihan <konrad.kihan@gmail.com>
2022-04-30 03:56:30 +0200

    PDO update work in progress

--- Projektowanie_stron_www-r1s2/Projekt_zaliczenie/add_to_database.php
+++ Projektowanie_stron_www-r1s2/Projekt_zaliczenie/add_to_database.php
@@ -1,28 +1,31 @@
 <?php
-    
-    function save_to_db(
-        $host = "localhost",
-        $user = "root",
-        $password = "",
-        $db = "umkwww"
-    ){
-        $connection = mysqli_connect($host, $user, $password, $db);
-        
+        $host = "51.83.180.200";
+        $dbname = "umkwww";
+        $dbusr = "podkowa";
+        $dbpsw = "zaq1@WSX";
+
+        $dsn = "pgsql=host:$host;port=5432;dbname=umkwww;";
+        $conn = new PDO($dsn, $dbusr, $dbpsw);
+
+
+        /*if(!$conn)
+	{
+                echo "AAAAAAAAAAAAAAA";
+                return -1;
+        }
+
+
+
+
         $phone = $_POST["phone"];
         $email = $_POST["email"];
         $name = $_POST["name"];
         $surname = $_POST["surname"];
 
-        $query = "INSERT INTO newsletter (`phone_number`, `email`, `name`, `surname`) VALUES('$phone', '$email', '$name', '$surname');";
+        $query = "INSERT INTO newsletter (phone_number, email, name, surname) VALUES($phone, '".$email."', '".$name."', '".$surname."');";
+	$stmt = $conn->prepare($query);
 
-        $result = mysqli_query($connection, $query);
+        $stmt->execute();*/
 
-        mysqli_close($connection);
-
-        header("Location: index.html");
-    }
-
-
-
-    save_to_db();
+        echo "Dziala";
 ?>
---
+++ Projektowanie_stron_www-r1s2/Projekt_zaliczenie/config.php
@@ -0,0 +1,8 @@
+<?php
+$host='51.83.180.200';
+$db='umkwww';
+//$user='kihan';
+$user='postgres';
+//$password='1234';
+$password='Konrad101_';
+?>
